006981b3928eaec9808635c378ccc6d22493b274
olga.varfolomeeva <olga.varfolomeeva@jetbrains.com>
2021-02-17 12:55:57 +0300

    frame test

---
+++ WpfApplication55/Tests/_Frame.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfApplication5.Tests
+{
+    public partial class _Frame : Window
+    {
+        public _Frame()
+        {
+            InitializeComponent();
+        }
+    }
+}
